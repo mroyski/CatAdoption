7489fe1624f2c2f3147bb4d1ef6dcca805bcc072
Michael Royski <mroyski@gmail.com>
2019-12-05 17:38:05 -0500

    adoptionsuccess method

--- CatAdoption/Controllers/CatController.cs
+++ CatAdoption/Controllers/CatController.cs
@@ -1,5 +1,7 @@
-﻿using CatAdoption.Repositories;
+﻿using CatAdoption.Models;
+using CatAdoption.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,22 +12,42 @@ namespace CatAdoption.Controllers
 {
     public class CatController : Controller
     {
-        ICatRepository catRepo;
-        public CatController(ICatRepository catRepo)
+        private readonly UserManager<IdentityUser> _userManager;
+        ICatRepository _catRepo;
+
+        public CatController(ICatRepository catRepo, UserManager<IdentityUser> userManager)
         {
-            this.catRepo = catRepo;
+            _catRepo = catRepo;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
-            var model = catRepo.GetAll();
+            var model = _catRepo.GetAll();
             return View(model);
         }
 
         [Authorize]
         public IActionResult Details(int id)
         {
-            var model = catRepo.GetById(id);
+            var model = _catRepo.GetById(id);
             return View(model);
+        }
+
+        [HttpGet("cat/adopt")]
+        [Authorize]
+        public async Task<IActionResult> AdoptAsync(int id)
+        {
+            var model = _catRepo.GetById(id);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return View(model);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public string AdoptSuccess(int id)
+        {
+            var model = _catRepo.GetById(id);
+            return $"Successfully adopted {model.Name}";
         }
     }
 }
