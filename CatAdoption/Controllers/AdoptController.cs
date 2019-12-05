1dac0eaf7b0f3eb670d86f2dbc9f0fba93dd492b
Michael Royski <mroyski@gmail.com>
2019-12-05 15:55:14 -0500

    adpot controller

---
+++ CatAdoption/Controllers/AdoptController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatAdoption.Controllers
+{
+    public class AdoptController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdoptController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        // GET: Adopt
+        public async Task<string> IndexAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return user.ToString();
+        }
+
+        // GET: Adopt/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+    }
+}
