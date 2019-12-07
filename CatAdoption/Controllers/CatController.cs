1a4be8ac38c9c2457e0422c75f7fcc96c5da431d
Michael Royski <mroyski@gmail.com>
2019-12-07 18:23:47 -0500

    can filter by gender

--- CatAdoption/Controllers/CatController.cs
+++ CatAdoption/Controllers/CatController.cs
@@ -1,7 +1,11 @@
-﻿using CatAdoption.Repositories;
+﻿using CatAdoption.Data;
+using CatAdoption.Models;
+using CatAdoption.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,20 +14,45 @@ namespace CatAdoption.Controllers
     public class CatController : Controller
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly CatDbContext _catContext;
         ICatRepository _catRepo;
         IAdoptRepository _adoptRepo;
 
-        public CatController(ICatRepository catRepo, IAdoptRepository adoptRepo, UserManager<IdentityUser> userManager)
+        public CatController(ICatRepository catRepo, IAdoptRepository adoptRepo, UserManager<IdentityUser> userManager, CatDbContext catContext)
         {
             _catRepo = catRepo;
             _userManager = userManager;
             _adoptRepo = adoptRepo;
+            _catContext = catContext;
 
         }
-        public IActionResult Index()
+        //public IActionResult Index()
+        //{
+        //    var model = _catRepo.GetAll();
+        //    return View(model);
+        //}
+
+        public async Task<IActionResult> Index(string catGender)
         {
-            var model = _catRepo.GetAll();
-            return View(model);
+            IQueryable<string> genderQuery = from g in _catContext.Cats
+                                             orderby g.Gender
+                                             select g.Gender;
+
+            var cats = from g in _catContext.Cats
+                       select g;
+
+            if (!string.IsNullOrEmpty(catGender))
+            {
+                cats = cats.Where(x => x.Gender == catGender);
+            }
+
+            var catGenderVM = new CatGenderViewModel
+            {
+                Genders = new SelectList(await genderQuery.Distinct().ToListAsync()),
+                Cats = await cats.ToListAsync()
+            };
+
+            return View(catGenderVM);
         }
 
         [Authorize]
---
+++ CatAdoption/Models/CatGenderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatAdoption.Models
+{
+    public class CatGenderViewModel
+    {
+        public List<Cat> Cats { get; set; }
+        public SelectList Genders { get; set; }
+        public string CatGender { get; set; }
+        public string Gender { get; set; }
+    }
+}
